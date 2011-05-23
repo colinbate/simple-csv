f34b3a653b28e218000fdec79454979454914895
Colin Bate <1060912+colinbate@users.noreply.github.com>
2011-05-23 00:46:50 -0300

    Adding the first couple of tools. One is a CSV parsing function and the other is an object merger.

---
+++ tools.js
@@ -0,0 +1,19 @@
+// 
+//  tools.js
+//  
+//  Created by Colin Bate on 2011-05-22.
+//  Copyright 2011 Colin Bate. All rights reserved.
+// 
+
+// Works similar to jQuery's $.extend() only it doesn't mutate any of the objects
+// Always returns a new object with the properties being writable and enumerable
+exports.merge = function () {
+	var newObj = {};
+	for (var arg = 0; arg < arguments.length; arg +=1) {
+		var keys = Object.keys(arguments[arg]);
+		for (var k in keys) {
+			Object.defineProperty(newObj, keys[k], {value: arguments[arg][keys[k]], writable: true, enumerable: true});
+		}
+	}
+	return newObj;
+};
